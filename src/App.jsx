565ab7f102dfccd791489d5e7fab78760329a108
Nzr1981 <nzr.ali1981@gmail.com>
2026-07-31 18:32:39 +0000

    Update Index Game Analyzer

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <div className="app">
+      <Sidebar />
+
+      <main className="content">
+        <Dashboard />
+      </main>
+    </div>
+  );
+}
+
+export default App;
