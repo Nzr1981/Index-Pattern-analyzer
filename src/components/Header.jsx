7087dc6eba965b627a741baa434cb13879a96a2f
Nzr1981 <nzr.ali1981@gmail.com>
2026-08-01 18:11:40 +0000

    App running successfully

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>INDEX GAME ANALYZER</h1>
+    </header>
+  );
+}
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>INDEX GAME ANALYZER</h1>
+      <p>Dashboard is working.</p>
+    </div>
+  );
+}
